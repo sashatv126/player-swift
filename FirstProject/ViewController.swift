7f564382bd8bc7522b68ef8d670b62b9600b90d2
sashatv126 <chekar@yandex.ru>
2022-02-19 15:28:40 +0300

    Initial Commit

---
+++ FirstProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstProject
+//
+//  Created by Владимир on 19.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
