279de2b362db56d9eb8a6a867ff9d6c48af29ae2
sashatv126 <sashatv126@gmail.com>
2022-02-21 21:22:57 +0300

    test 2

---
+++ FirstProject/Cells/CellOfFavouriteMusicViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  CellOfFavouriteMusicViewModel.swift
+//  FirstProject
+//
+//  Created by Владимир on 21.02.2022.
+//
+
+import Foundation
---
+++ FirstProject/Cells/CellOfMusicViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  CellOfMusicViewModel.swift
+//  FirstProject
+//
+//  Created by Владимир on 21.02.2022.
+//
+
+import Foundation
