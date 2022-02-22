fc4574f13061f5055b1f3087377bc191b41f78e7
sashatv126 <sashatv126@gmail.com>
2022-02-22 23:15:39 +0300

    add check

--- FirstProject/Extension/ValidTypes.swift
+++ FirstProject/Extension/ValidTypes.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+extension String {
+    enum ValidTypes {
+        case name
+        case email
+        case password
+    }
+    enum Regex : String {
+        case name = "[a-zA-Z]{3,}"
+        case email = "[a-zA-Z0-9]+@[a-zA-Z]+\\.[a-zA-z]{2,}"
+        case password = "[a-zA-Z0-9]{6,12}"
+    }
+    
+    func isValid(validTypes : ValidTypes) -> Bool {
+        let format = "SELF MATCHES %@"
+        var regex = ""
+        
+        switch validTypes {
+        case .name: regex = Regex.name.rawValue
+        case .email: regex = Regex.email.rawValue
+        case .password: regex = Regex.password.rawValue
+        }
+        return NSPredicate(format: format,regex).evaluate(with: self)
+    }
+}
+
--- FirstProject/SingUpViewController.swift
+++ FirstProject/SingUpViewController.swift
@@ -8,6 +8,9 @@
 import UIKit
 import SnapKit
 class SignUpViewController: UIViewController {
+    let validName = String.ValidTypes.name
+    let validEmail = String.ValidTypes.email
+    let validPassword = String.ValidTypes.password
 //MARK: Views
     private let scrollView : UIScrollView = {
         let scrollView = UIScrollView()
@@ -40,6 +43,18 @@ class SignUpViewController: UIViewController {
         textField.isSecureTextEntry = true
         return textField
     }()
+    private let labelValidname : UILabel = {
+       let label = UILabel()
+        return label
+    }()
+    private let labelValidEmail : UILabel = {
+       let label = UILabel()
+        return label
+    }()
+    private let labelValidPassword : UILabel = {
+       let label = UILabel()
+        return label
+    }()
     private let registerButton : UIButton = {
         let button = UIButton(type: .system)
         button.backgroundColor = .black
@@ -67,8 +82,11 @@ class SignUpViewController: UIViewController {
             title = "Registration"
             view.backgroundColor = .white
             textFieldStackView.addArrangedSubview(userNameTextField)
+            textFieldStackView.addArrangedSubview(labelValidname)
             textFieldStackView.addArrangedSubview(emailTextField)
+            textFieldStackView.addArrangedSubview(labelValidEmail)
             textFieldStackView.addArrangedSubview(passwordTextField)
+            textFieldStackView.addArrangedSubview(labelValidPassword)
             textFieldStackView.addArrangedSubview(repeatedPasswordTextField)
             view.addSubview(scrollView)
             scrollView.addSubview(textFieldStackView)
@@ -81,6 +99,26 @@ class SignUpViewController: UIViewController {
             repeatedPasswordTextField.delegate = self
         }
     @objc private func registerTapped() {
+    }
+    private func setTextField(textField : UITextField,label : UILabel, validType : String.ValidTypes,validMessage : String, wrongMessage : String, string : String , range : NSRange) {
+        let text = (textField.text ?? "") + string
+        let result : String
+        if range.length == 1 {
+            let end = text.index(text.startIndex, offsetBy: text.count-1)
+            result = String(text[text.startIndex..<end])
+        }
+        else {
+            result = text
+        }
+        textField.text = result
+        if result.isValid(validTypes: validType) {
+            label.text = validMessage
+            label.textColor = .green
+        }
+        else {
+            label.text = wrongMessage
+            label.textColor = .red
+        }
     }
 //MARK: -Constraints
      private func setupConstraints() {
@@ -114,4 +152,32 @@ class SignUpViewController: UIViewController {
             repeatedPasswordTextField.resignFirstResponder()
             return true
         }
+        func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+            switch textField {
+            case userNameTextField : setTextField(textField: textField,
+                                                    label: labelValidname,
+                                                    validType: validName,
+                                                    validMessage: "Valid username",
+                                                    wrongMessage: "only use a-z and A-z",
+                                                    string: string,
+                                                    range: range)
+            case emailTextField : setTextField(textField: textField,
+                                                    label: labelValidEmail,
+                                                    validType: validEmail,
+                                                    validMessage: "Valid email",
+                                                    wrongMessage: "for example example@web.com",
+                                                    string: string,
+                                                    range: range)
+            case passwordTextField : setTextField(textField: textField,
+                                                    label: labelValidPassword,
+                                                    validType: validPassword,
+                                                    validMessage: "Valid password",
+                                                    wrongMessage: "min 8 characters",
+                                                    string: string,
+                                                    range: range)
+            default : break
+            }
+            return false
+        }
+        
     }
