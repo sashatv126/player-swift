b0c410ad6a4fb505bede5ae20872308ba607f89c
sashatv126 <sashatv126@gmail.com>
2022-02-22 16:42:48 +0300

    End of Visual

--- FirstProject/Cells/CellOfFavouriteMusicViewModel.swift
+++ FirstProject/Cells/CellOfFavouriteMusicViewModel.swift
@@ -12,3 +12,4 @@ struct CellOfFavouriteMusicViewModel {
     let cover : UIImage?
     let nameOfArtist : String
 }
+let arrayOfFavMusic : [CellOfFavouriteMusicViewModel] = [CellOfFavouriteMusicViewModel(nameOfSong: "lll", cover: Constants.Image.contentBlock, nameOfArtist: "lllll")]
