7406653644f31834af6f07afaaa2be0258de7bcb
sashatv126 <sashatv126@gmail.com>
2022-02-22 10:30:40 +0300

    add some new

---
+++ FirstProject/PlayViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PlayViewController.swift
+//  FirstProject
+//
+//  Created by Владимир on 22.02.2022.
+//
+
+import UIKit
+
+class PlayViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
