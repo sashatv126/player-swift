1630382bdccdadf8a73a777f0f8d6dbd575128e8
sashatv126 <sashatv126@gmail.com>
2022-02-24 15:03:01 +0300

    test 4

--- FirstProject/Extension/AlertController.swift
+++ FirstProject/Extension/AlertController.swift
@@ -5,4 +5,23 @@
 //  Created by Владимир on 23.02.2022.
 //
 
-import Foundation
+import UIKit
+
+extension UIViewController {
+    
+    func aleartController (title : String, message : String, action : String){
+        let alert  = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let dis = UIAlertAction(title: "OK", style: .default, handler: {_ in self.dismiss(animated: true, completion: nil)})
+        let ok = UIAlertAction(title: "OK", style: .default)
+        switch action {
+        case "dismiss": alert.addAction(dis)
+        case "OK" : alert.addAction(ok)
+        default:
+            break
+        }
+        present(alert,animated: true, completion: nil)
+   }
+}
+enum Alerts {
+    
+}
