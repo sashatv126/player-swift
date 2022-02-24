25c62fd9abc00309a03761ce4f79a7972fd59fce
sashatv126 <sashatv126@gmail.com>
2022-02-24 15:22:01 +0300

    test 2

--- FirstProject/Services /UserDefaultsManager .swift
+++ FirstProject/Services /UserDefaultsManager .swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+
+class DataBase {
+    
+    static let shared = DataBase()
+    
+    enum SettingKeys : String {
+        case user
+    }
+    let defaults = UserDefaults.standard
+    let userKey = SettingKeys.user.rawValue
+    var users : [User] {
+        get {
+            if let data = defaults.value(forKey: userKey) as? Data {
+                return try! PropertyListDecoder().decode([User].self, from: data)
+            }
+            else {
+                return [User]()
+            }
+        }
+        set {
+            if let data = try? PropertyListEncoder().encode(newValue) {
+                defaults.set(data,forKey: userKey)
+            }
+        }
+    }
+    func saveUsers(username : String,email : String , password : String , repeatPassword : String)  {
+        
+        let user = User(usrname: username, email: email, password: password, repeatPassword: repeatPassword)
+        users.append(user)
+    }
+}
